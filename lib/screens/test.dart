ac8064af1914ea3d4f81acf6c08eb1e349acf139
Bryan <bryantham61@gmail.com>
2025-03-23 17:51:28 +0800

    test

---
+++ lib/screens/test.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'parking_layout.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Page')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Hello World!',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ParkingLayout()),
+                );
+              },
+              child: const Text('Go To Parking Page'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
